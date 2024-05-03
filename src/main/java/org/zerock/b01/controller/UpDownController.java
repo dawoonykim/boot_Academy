ac823831c78f857132f470174f8a28a7c9298854
ramykim <dawoonk111@gmail.com>
2024-05-03 16:48:01 +0900

    625P 게시물과 첨부파일 삭제 전까지 작성

--- src/main/java/org/zerock/b01/controller/UpDownController.java
+++ src/main/java/org/zerock/b01/controller/UpDownController.java
@@ -137,4 +137,5 @@ public class UpDownController {
 
         return resultMap;
     }
+
 }
--- src/main/java/org/zerock/b01/dto/upload/UploadResultDTO.java
+++ src/main/java/org/zerock/b01/dto/upload/UploadResultDTO.java
@@ -24,4 +24,5 @@ public class UploadResultDTO {
             return uuid + "_" + fileName;
         }
     }
+
 }
